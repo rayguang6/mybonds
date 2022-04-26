8aac71362fd33c3c99559af3e3cfafffc52ed7d0
Lei Zhi Guang <rayguang123@gmail.com>
2022-04-26 15:48:03 +0800

    remove default active state

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 
 // Function to set active class on list item menu
+
+// const currentLocation = location.href;
+// console.log('currentlocation = '+currentLocation);
+// const menuItem = document.querySelectorAll('.menu_a');
+// const menuLength = menuItem.length;
+// for(let i = 0; i< menuLength; i++){
+//     menuItem[i].classList.remove("active");
+//     if(menuItem[i].href === currentLocation){
+//         // console.log('menu item: '+ menuItem[i]);
+//         // console.log("menu href: " + menuItem[i].href);
+//         menuItem[i].className += " active";
+//     }
+// }
+
 const currentLocation = location.href;
 console.log('currentlocation = '+currentLocation);
 const menuItem = document.querySelectorAll('.menu_a');
