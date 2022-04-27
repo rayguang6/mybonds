9c36ff9058cf67dc5270aee5aa0440a520b1c1cf
Lei Zhi Guang <rayguang123@gmail.com>
2022-04-27 12:22:25 +0800

    added monthly chart to maagecovid

--- assets/js/managecovid.js
+++ assets/js/managecovid.js
@@ -579,3 +579,42 @@ const myChart = new Chart(ctx, {
         }
     }
 });
+
+
+const monthlyCasesChart = document.getElementById('monthlyCasesChart').getContext('2d');
+const monthlyChart = new Chart(monthlyCasesChart, {
+    type: 'bar',
+    data: {
+        labels: ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26'],
+        datasets: [{
+            label: '# of Cases',
+            data: [12,2,4,6,2,7,8,2,6,5,12,16,5,3,4,8,9,6,5,6,4,3,4,8,6,2],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(13, 110, 253, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(13, 110, 253, 1)',
+                'rgba(153, 102, 255, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
